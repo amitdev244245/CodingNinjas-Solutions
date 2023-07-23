a15f6f2ab1c788e89a9aa54bcd6be38327f09bb2
Amit Sharma <amitdev244245@gmail.com>
2023-07-24 02:04:17 +0530

    added switch case program

---
+++ C++/switch-case-statement.cpp
@@ -0,0 +1,23 @@
+// Problem Name: Switch Case statement
+// Problem Level: Easy
+
+/*
+    Time complexity: O(1)
+    Space complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+double areaSwitchCase(int ch, vector<double> a)
+{
+    switch (ch)
+    {
+    case 1:
+        return 3.14159265359 * a[0] * a[0];
+        break;
+    case 2:
+        return a[0] * a[1];
+        break;
+    }
+}
