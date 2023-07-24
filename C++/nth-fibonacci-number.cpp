6aae6d431b92099cb8336c925b98156e38ff4b67
Amit Sharma <amitdev244245@gmail.com>
2023-07-25 03:29:06 +0530

    added nth fibonacci number  program

---
+++ C++/nth-fibonacci-number.cpp
@@ -0,0 +1,31 @@
+// Problem Name: Nth Fibonacci Number
+// Problem Level: Easy
+
+/*
+    ime complexity: O(2^N)
+    Space complexity: O(N)
+    Where 'N' reprents the "Nth" number .
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n)
+{
+    if (n <= 1)
+        return n;
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main()
+{
+    /*
+     *  Write your code here.
+     *  Read input as specified in the question.
+     *  Print output as specified in the question.
+     */
+    int n;
+    cin >> n;
+    cout << fibo(n);
+    return 0;
+}
