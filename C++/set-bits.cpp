d9d11aac6cdabea7696047880083a1eba74d4767
Amit Sharma <amitdev244245@gmail.com>
2023-07-25 03:54:31 +0530

    added set-bits program

---
+++ C++/set-bits.cpp
@@ -0,0 +1,27 @@
+// Problem Name: Set Bits
+// Problem Level: Easy
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+// Write your countBits function here
+int countBits(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << countBits(n);
+    return 0;
+}
