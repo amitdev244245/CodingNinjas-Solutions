7eca6f832939caf93b3199d19087d5c3c197f897
Amit Sharma <amitdev244245@gmail.com>
2023-07-28 23:03:24 +0530

    added program function-to-print-hello-world in C++

---
+++ C++/function-to-print-hello-world.cpp
@@ -0,0 +1,17 @@
+// Problem Name: Function to print Hello World
+// Problem Level: Easy
+
+#include <iostream>
+using namespace std;
+
+// Write the print function here
+string print(string s)
+{
+    return s;
+}
+
+int main()
+{
+    cout << print("Hello World");
+    return 0;
+}
