719d868a402ec2534ddd5c31fc272b5e5e25b7a4
Amit Sharma <amitdev244245@gmail.com>
2023-08-01 21:04:47 +0530

    Added star-pattern in C++

---
+++ C++/star-pattern.cpp
@@ -0,0 +1,21 @@
+// Problem Name: Star Pattern
+// Problem Level: Easy
+
+#include <bits/stdc++.h>
+
+void printPattern(int n)
+{
+    // Write your code here.
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i * 2 - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
