81e358b761f16f8eda91416fe4bb13c9c62e795b
Amit Sharma <amitdev244245@gmail.com>
2023-08-07 22:03:09 +0530

    Added program count-words in C++

---
+++ C++/count-words.cpp
@@ -0,0 +1,31 @@
+// Problem Name: Count Words
+// Problem Level: Easy
+// Problem Link: https://www.codingninjas.com/studio/guided-paths/basics-of-c/content/118818/offering/1381763?leftPanelTab=0
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int countWords(string input)
+{
+    // Write your code here
+    if (input.size() == 0)
+        return 0;
+    int count = 1;
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (input[i] == ' ')
+            count += 1;
+    }
+    return count;
+}
+
+int main()
+{
+    string input;
+    getline(cin, input);
+    int output = countWords(input);
+    cout << output << endl;
+    return 0;
+}
