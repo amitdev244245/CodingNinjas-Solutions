a2d9e2c9ee03b2abfe19ee826818108ead1664c3
Amit Sharma <amitdev244245@gmail.com>
2023-08-10 23:20:49 +0530

    Updated program link last-index-of-element in C++

--- C++/last-index-of-element.cpp
+++ C++/last-index-of-element.cpp
@@ -1,5 +1,6 @@
 // Problem Name: Last index of element
 // Problem Level: Easy
+// Problem Link: https://www.codingninjas.com/studio/guided-paths/basics-of-c/content/118787/offering/1381277
 
 #include <bits/stdc++.h>
 #include <iostream>
