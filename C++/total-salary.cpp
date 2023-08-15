7bf40f398814182dd84f28802160bd16538ce13a
Amit Sharma <amitdev244245@gmail.com>
2023-08-15 23:19:37 +0530

    Updated program link total-salary in C++

--- C++/total-salary.cpp
+++ C++/total-salary.cpp
@@ -1,37 +1,25 @@
 // Problem Name: Total Salary
 // Problem Level: Easy
+// Problem Link: https://www.codingninjas.com/studio/problems/total-salary_893200
 
 #include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
-
-int main()
+int totalSalary(int basic, char grade)
 {
-    // Write your code here
-    double basic, allow, totalSalary, hra, da, pf;
-    char grade;
-
-    cin >> basic >> grade;
+    double allow, totalSalary, hra, da, pf;
 
     hra = basic * 0.5;
     da = basic * 0.2;
     pf = basic * 0.11;
 
     if (grade == 'A')
-    {
         allow = 1700;
-    }
     else if (grade == 'B')
-    {
         allow = 1500;
-    }
     else
-    {
         allow = 1300;
-    }
 
     totalSalary = basic + hra + da + allow - pf;
     int ans = round(totalSalary);
-    cout << ans;
-    return 0;
+
+    return ans;
 }
